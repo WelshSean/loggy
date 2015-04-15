497fc3462d3ca2080e1d462dae93495efbfb810b
Sean Clarke <sean@nay-clarke.com>
2015-04-15 23:14:24 +0100

    Loggy can now pick a message by OS and Index

--- loggy.py
+++ loggy.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python
 
-import json
+import json,sys
 from pprint import pprint
+
+def pickMessage(data, OS, Index):
+    print data[OS][Index]
 
 with open('loggy.data') as data_file:    
     data = json.load(data_file)
 
-print data["Linux"]
+## Example of data format
+## print data["Linux"]["Lin1"]
+
+
+if  sys.argv[1] == "Random":
+    pickMessage(data, sys.argv[2], sys.argv[3])
+
+
